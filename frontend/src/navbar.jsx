1fdb35ee99cdbf6c5d063a68b1e064fd210a82d2
YuranJo <y14r06a062006@gmail.com>
2024-04-07 02:40:03 +0300

    +navbar

---
+++ frontend/src/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div id="nav_div">
+        <nav>
+            
+        <ul>
+            <li><a href="#home" id="home-link">Home</a></li>
+            <li><a href="#catalog" id="catalog-link">Catalog</a></li>
+            <li><a href="#about" id="about-link">About</a></li>
+            <li><a href="#contacts" id="contacts-link">Contacts</a></li>
+        </ul>
+
+        </nav>
+    </div>
+  );
+};
+
+export default NavBar;
