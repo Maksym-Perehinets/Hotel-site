205f6390d76da9d3fce4de7fbf52d269881789a3
YuranJo <y14r06a062006@gmail.com>
2024-04-09 22:27:24 +0300

    added profilepic

--- frontend/src/navbar.jsx
+++ frontend/src/navbar.jsx
@@ -6,12 +6,16 @@ const NavBar = () => {
         <nav>
             
         <ul>
+            <li><img id="logo" src="#" alt="hui" srcset="" /></li>
             <li><a href="#home" id="home-link">Home</a></li>
             <li><a href="#catalog" id="catalog-link">Catalog</a></li>
             <li><a href="#about" id="about-link">About</a></li>
             <li><a href="#contacts" id="contacts-link">Contacts</a></li>
+            
         </ul>
 
+        <img id="profilePic" alt="hui2" srcset="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6916fee6-5951-4c7c-be7f-ff7c6002f469/d46gysz-5bc7eb26-98fd-41db-93e9-6c9b44a8752a.png/v1/fit/w_700,h_500,q_70,strp/homestuck_cats_by_ansemaru_d46gysz-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTAwIiwicGF0aCI6IlwvZlwvNjkxNmZlZTYtNTk1MS00YzdjLWJlN2YtZmY3YzYwMDJmNDY5XC9kNDZneXN6LTViYzdlYjI2LTk4ZmQtNDFkYi05M2U5LTZjOWI0NGE4NzUyYS5wbmciLCJ3aWR0aCI6Ijw9NzAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.1JGYLCn6vDWVXIOU7W5zYzOW9NQ9BxIqZ8l6MvbbeN0"></img>
+        
         </nav>
     </div>
   );
