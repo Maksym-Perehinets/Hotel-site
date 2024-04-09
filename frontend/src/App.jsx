dc3d6c13fa132c20b9d9b1f64c248649b18cec6e
YuranJo <y14r06a062006@gmail.com>
2024-04-09 23:32:44 +0300

    hui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import NavBar from './navbar'
 import './navbar.css'
+import Home from './home.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,8 @@ function App() {
   return (
     <>
       <NavBar />
+      <Home/>
+      
 
     </>
   )
---
+++ frontend/src/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './home.css';
+
+const home  = () => {
+    return (
+        <>
+            <div id='main'>
+                <div id="info-div">
+                    hui
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default home;
