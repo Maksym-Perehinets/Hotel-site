6c01e03ae9231b88345c39851b0e3b67c4716912
Perehinets Maksym <supercomf128@gmail.com>
2024-04-13 17:07:21 +0300

    Update of api. Added db to docker-compose.yml and init.sql

--- backend/app/routers/home_page.py
+++ backend/app/routers/home_page.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/rooms/v1/{items_on_page}", tags=["home page info"])
+@router.get("/v1/rooms/{items_on_page}", tags=["home page info"])
 async def get_rooms(items_on_page: int):
     return {
             "message": "good",
