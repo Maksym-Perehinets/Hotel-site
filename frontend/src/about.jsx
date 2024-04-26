ade203b0fe0962b28a8e9f0a4fde858d1d02afaf
chang31ing <azrieldreemur2006@gmail.com>
2024-04-26 16:25:48 +0300

    fonts + colors + parallax picture

---
+++ frontend/src/about.jsx
@@ -0,0 +1,14 @@
+import Navbar from './navbar'
+import React from 'react';
+import './App.css'
+
+const About = () => {
+    return (
+    <>
+    <div>hellow orld</div>
+    
+    </>
+    );
+};
+
+export default About;
